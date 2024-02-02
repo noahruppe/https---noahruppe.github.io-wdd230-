f0434327c05af768be019206291100b03c6d758e
noahruppe <noah.ruppe30@gmail.com>
2024-02-02 17:31:37 -0500

    save

---
+++ chamber/scripts/responsive.js
@@ -0,0 +1,18 @@
+const options = {year: 'numeric'}
+document.getElementById('currentYear').textContent =new Date().toLocaleDateString('en-US',options);
+
+
+const modified = {day: 'numeric',month: 'numeric',year: 'numeric',hour: 'numeric', minute: 'numeric'}
+let oLastModified = new Date(document.lastModified);
+document.getElementById('lastModified').textContent = oLastModified.toLocaleDateString('en-US',modified)
+
+
+
+const mainnav = document.querySelector('.link')
+const hambutton = document.querySelector('#menu')
+
+hambutton.addEventListener('click',() =>{
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+
+})
