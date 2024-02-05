f33a4abbf3787d0361876fa3d8db167bb1cbed33
noahruppe <noah.ruppe30@gmail.com>
2024-02-05 18:49:53 -0500

    this

---
+++ lesson05/script/reference.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    if (input.value.trim() !== "") {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        deleteButton.style.marginLeft = '30px';
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.focus();
+        input.value = '';
+    } else {
+        alert('Please enter a book and chapter.');
+        input.focus();
+    }
+});
