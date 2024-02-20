d2f0c827ae44752561445de45a5dfa6a110cee92
noahruppe <noah.ruppe30@gmail.com>
2024-02-20 18:01:54 -0500

    save

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -6,3 +6,18 @@ hambutton.addEventListener('click',() =>{
     hambutton.classList.toggle('show');
 
 })
+
+ const visitsDisplay = document.querySelector('.visits')
+ 
+ let numVisits = Number(window.localStorage.getItem('numVisits-ls')) || 0;
+
+ if(numVisits !== 0 ){
+    visitsDisplay.textContent = numVisits
+ }
+ else{
+    visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+ }
+
+ numVisits++;
+
+ localStorage.setItem("numVisits-ls", numVisits);
