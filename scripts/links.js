1187130a861eee2619bbbc14ecec44d4eb980256
noahruppe <noah.ruppe30@gmail.com>
2024-03-05 21:23:48 -0500

    save

--- scripts/links.js
+++ scripts/links.js
@@ -1,13 +1,27 @@
-const baseUrl = "https://github.com/noahruppe/wdd230";
-
-const linksUrl = "https://github.com/noahruppe/wdd230/blob/main/data/links.json";
+const baseUrl = "https://noahruppe.github.io/wdd230/";
+const linksUrl = "https://noahruppe.github.io/wdd230/data/links.json";
 
 const cards = document.querySelector('#cards');
 
-async function getLinks(){
-    const response = await fetch(baseUrl+linksUrl);
+async function getLinks() {
+    const response = await fetch(linksUrl);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data.lessons);
 }
+
+const displayLinks = (lessons) => {
+    lessons.forEach((lesson) => {
+        lesson.links.forEach((linkData) => {                           //from this line down I had help with chat gpt  
+            let card = document.createElement("section");
+            let link = document.createElement("a");
+
+            link.setAttribute('href', baseUrl + linkData.url);
+            link.textContent = linkData.title;
+
+            card.appendChild(link);
+            cards.appendChild(card);
+        });
+    });
+};
 
 getLinks();
