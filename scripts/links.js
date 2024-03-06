1effad87834554c4b7eed1126769b90e4e689563
noahruppe <noah.ruppe30@gmail.com>
2024-03-05 20:35:40 -0500

    sa

--- scripts/links.js
+++ scripts/links.js
@@ -1,3 +1,13 @@
-const baseUrl = "https://github.com/noahruppe/wdd230/"
+const baseUrl = "https://github.com/noahruppe/wdd230";
 
-const linksUrl = "data/links.json"
+const linksUrl = "https://github.com/noahruppe/wdd230/blob/main/data/links.json";
+
+const cards = document.querySelector('#cards');
+
+async function getLinks(){
+    const response = await fetch(baseUrl+linksUrl);
+    const data = await response.json();
+    console.log(data);
+}
+
+getLinks();
