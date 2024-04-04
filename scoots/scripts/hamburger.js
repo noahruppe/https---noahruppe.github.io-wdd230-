3375260b7c4361d752d851f851b4c93a0c80ef86
noahruppe <noah.ruppe30@gmail.com>
2024-04-04 17:45:24 -0400

    s

--- scoots/scripts/hamburger.js
+++ scoots/scripts/hamburger.js
@@ -17,3 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const location = document.querySelector('.location');
     location.style.height = '500';
 });
+
+function redirectToReservations() {
+    window.location.href = 'https://noahruppe.github.io/wdd230/scoots/reservations.html';
+}
